7dcd53a02cb510bddd6cac3cc9ee6a86bf393fa9
Perpetua Mary <perpetuamary@yahoo.com>
2019-04-28 11:31:49 +0300

    introduced a function that askes user to input values

---
+++ js/triangle-tracker.js
@@ -0,0 +1,7 @@
+function myTriangleTracker(){
+ var length;
+ var length1= parseInt(document.getElementById("number1").value);
+ var length2= parseInt(document.getElementById("number2").value);
+ var length3= parseInt(document.getElementById("number3").value);
+
+}
