b77736894bace5e9d58eec956885e08ecdf4c55b
Perpetua Mary <perpetuamary@yahoo.com>
2019-04-28 13:03:21 +0300

    add an if else statement that helps to determine whether the values added are make isosceles triangle, equilateral triangle, scalene triangle or does not make a triangle at all.

--- js/triangle-tracker.js
+++ js/triangle-tracker.js
@@ -1,7 +1,21 @@
 function myTriangleTracker(){
- var length;
  var length1= parseInt(document.getElementById("number1").value);
  var length2= parseInt(document.getElementById("number2").value);
  var length3= parseInt(document.getElementById("number3").value);
+ if (((length1 + length2) > length3) && ((length1 + length3) > length2) && ((length2 + length3) > length1)) {
+   if ((length1 === length2 && length1 !== length3) || (length1 === length3 && length1 !== length2) || (length2 === length3 && length2 !== length1)) {
 
-}
+     document.getElementById('display').innerHTML = "This is an Isosceles Triangle";
+   } else if ((length1 === length2) && (length2 === length3)) {
+
+     document.getElementById('display').innerHTML = "This is an Equilateral Triangle";
+   } else {
+
+     document.getElementById('display').innerHTML = "This is a Scalene Triangle";
+   }
+ } else {
+
+   document.getElementById('display').innerHTML = "Those values cannot form a triangle";
+ }
+
+ }
