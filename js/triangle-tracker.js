ceef883e799923037a84f73dd8bd8d6833ff4ca4
Perpetua Mary <perpetuamary@yahoo.com>
2019-04-28 19:42:16 +0300

    add a new function in the javascript file.

--- js/triangle-tracker.js
+++ js/triangle-tracker.js
@@ -1,21 +1,25 @@
 function myTriangleTracker(){
- var length1= parseInt(document.getElementById("number1").value);
- var length2= parseInt(document.getElementById("number2").value);
- var length3= parseInt(document.getElementById("number3").value);
- if (((length1 + length2) > length3) && ((length1 + length3) > length2) && ((length2 + length3) > length1)) {
-   if ((length1 === length2 && length1 !== length3) || (length1 === length3 && length1 !== length2) || (length2 === length3 && length2 !== length1)) {
-     
-     document.getElementById('display').innerHTML = "This is an Isosceles Triangle";
-   } else if ((length1 === length2) && (length2 === length3)) {
-
-     document.getElementById('display').innerHTML = "That is an Equilateral Triangle";
-   } else {
-
-     document.getElementById('display').innerHTML = "That is a Scalene Triangle";
+  var length1= parseInt(document.getElementById("number1").value);
+  var length2= parseInt(document.getElementById("number2").value);
+  var length3= parseInt(document.getElementById("number3").value);
+  if (((length1 + length2) > length3) && ((length1 + length3) > length2) &&((length2 + length3) > length1)) {
+    if ((length1 === length2 && length1 !== length3) || (length1 === length3 && length1 !== length2) || (length2 === length3 && length2 !== length1)) {
+        document.getElementById('display').innerHTML = "This is an Isosceles Triangle";
+     }
+    else if ((length1 === length2) && (length2 === length3)) {
+        document.getElementById('display').innerHTML = "That is an Equilateral Triangle";
+     }
+    else {
+        document.getElementById('display').innerHTML = "That is a Scalene Triangle";
+     }
    }
- } else {
+   else {
+     document.getElementById('display').innerHTML = "Those values cannot form a triangle";
+   }
 
-   document.getElementById('display').innerHTML = "Those values cannot form a triangle";
  }
 
- }
+function myTriangle() {
+  var triangle = ["Equilateral", "Isosceles", "Scalene"];
+  alert(triangle.join()) ;
+}
